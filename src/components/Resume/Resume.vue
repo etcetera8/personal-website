51d8c3575f44bbd66e2098f35a121be223c166c3
parker lindley <fugazi8@gmail.com>
2018-03-31 12:48:28 -0600

    Add resume

--- src/components/Resume/Resume.vue
+++ src/components/Resume/Resume.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>RESUME</div>
+  <div class="resume-wrapper">
+    <div class="resume"></div>
+  </div>
 </template>
+
+<style>
+  .resume-wrapper {
+    display: flex;
+    justify-content: center;
+    padding: 25px;
+    margin: 25px;
+    background: #383D3B;
+  }
+
+  .resume {
+    width: 700px;
+    height: 900px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-image: url('../projects/assets/resume.png'); 
+  }
+</style>
+
