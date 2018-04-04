e5cfc95434aa0d75011abee50eb5489505b54e4a
parker lindley <fugazi8@gmail.com>
2018-04-04 12:52:37 -0600

    Update server to support history and refresh

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
-
+const history = require('connect-history-api-fallback');
 const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(serveStatic(__dirname + "/dist"));
-const  port = process.env.PORT || 5000;
+app.use(staticFileMiddleware);
+app.use(history({
+  disabledDotRule: true,
+  verbose: true
+}))
+app.use(staticFileMiddleware);
 
-app.listen(port);
-console.log('server started '+ port);
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+const server = app.listen(process.env.PORT || 5000, function () {
+  const port = server.address().port;
+  console.log("App now running on port", port);
+});
