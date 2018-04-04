7b7067e81c5b0fb09277255ce549a289dfc24130
parker lindley <fugazi8@gmail.com>
2018-04-04 13:23:58 -0600

    Roll back to original server

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ const path = require('path');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
-app.use(staticFileMiddleware);
-app.use(history({
-  disabledDotRule: true,
-  verbose: true
-}))
-app.use(staticFileMiddleware);
+app.use(serveStatic(__dirname + "/dist"));
+const  port = process.env.PORT || 5000;
 
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
-});
-
-const server = app.listen(process.env.PORT || 5000, function () {
-  const port = server.address().port;
-  console.log("App now running on the port", port);
-});
+app.listen(port);
+console.log('server started '+ port);
