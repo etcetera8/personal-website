c9ad0117409166e224299fea97bf1076b5d627f8
parker lindley <fugazi8@gmail.com>
2018-04-17 20:42:10 -0600

    Delete semantic update backend to work with vue router

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+// const express = require('express');
+// const path = require('path');
+// const serveStatic = require('serve-static');
 
-const app = express();
+// const app = express();
 
-app.use(serveStatic(__dirname + "/dist"));
-const  port = process.env.PORT || 5000;
+// app.use(serveStatic(__dirname + "/dist"));
+// const  port = process.env.PORT || 5000;
 
-app.listen(port);
-console.log('server started '+ port);
+// app.listen(port);
+// console.log('server started '+ port);
+
+
+var history = require('connect-history-api-fallback');
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+//add this middleware
+app.use(history());
+app.use(serveStatic(__dirname + "/dist"))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port) 
