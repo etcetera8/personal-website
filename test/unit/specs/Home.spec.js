9ae4c2da6fe6b02f180b2290d4acb4290a988745
parker lindley <fugazi8@gmail.com>
2018-05-07 19:45:23 -0600

    Update test to reflect changes

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -5,6 +5,6 @@ describe('HelloWorld.vue', () => {
     const Constructor = Vue.extend(Home)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hi').textContent)
-      .toEqual('Hi,')
+      .toEqual("I am an energetic front-end developer with a passion for learning and a drive for creating beautiful, functional and accessible apps. I have developed numerous web and mobile apps and have a background in cartography and GIS.")
   })
 })
